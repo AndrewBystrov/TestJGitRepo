595953651e9fe7d10126f37ae6aa1b7b75eab949
andrey.bystrov <andrey.bystrov@exactprosystems.com>
2017-03-20 10:14:24 +0300

    asd

---
+++ merge/firstRowMerge.sh
@@ -0,0 +1,18 @@
+#define
+file=$(sed "s/sh/txt/g" <<<$0)
+dateVar=$(date +%s%N)
+
+if [[ "$(pwd)" == *GIT_1* ]]
+then
+	countSecond=$2
+else
+	countSecond=$1
+fi
+countFirst=$1;
+
+for((i=0; i<"$countFirst"; i++))
+do
+	echo $(date +%s%N) >> $file
+done
+
+echo "End file $file" >> $file
