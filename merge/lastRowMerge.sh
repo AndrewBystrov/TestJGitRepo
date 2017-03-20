502e3855e1e3f016efa42ec31f86015d1c9171ce
andrey.bystrov <andrey.bystrov@exactprosystems.com>
2017-03-20 10:25:35 +0300

    add last merge script

---
+++ merge/lastRowMerge.sh
@@ -0,0 +1,19 @@
+#define
+file=$(sed "s/sh/txt/g" <<<$0)
+dateVar=$(date +%s%N)
+
+if [[ "$(pwd)" == *GIT_1* ]]
+then
+	countSecond=$2
+else
+	countSecond=$1
+fi
+countFirst=$1;
+
+echo "Start file $file" >> $file
+
+for((i=0; i<"$countFirst"; i++))
+do
+	echo $(date +%s%N) >> $file
+done
+
